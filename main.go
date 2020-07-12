15b1642ddf6d20a860fd42e275ce0274b1d0c844
Mark Bradley <braddle@gmail.com>
2020-07-12 21:28:46 +0100

    Setting up port for heroku

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/braddle/go-http-template/app"
 	"github.com/gorilla/mux"
@@ -9,5 +10,5 @@ import (
 
 func main() {
 	a := app.New(mux.NewRouter())
-	log.Fatal(a.Run(":8080"))
+	log.Fatal(a.Run(":" + os.Getenv("PORT")))
 }
